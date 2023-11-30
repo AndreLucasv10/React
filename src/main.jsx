8bb97808b4e0ee4332586fe87b60961aeb1bf8c1
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2023-11-29 21:19:57 -0300

    mini estudos

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./exercicio.jsx";
+import App from "./Jsx-Arrays.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
