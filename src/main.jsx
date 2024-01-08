82e34cb1154a04c2319451e12b75aa6334976b4a
André Lucas <88734289+AndreLucasv10@users.noreply.github.com>
2024-01-08 13:32:51 -0300

    commit esquecido

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./UseState/App";
+import App from "./UseState/useStateExercicio";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
